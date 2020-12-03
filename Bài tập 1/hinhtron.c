e719eee5054588de7039500d3e9d90ca4aa93255
anhvu2k2asd <72533371+anhvu2k2asd@users.noreply.github.com>
2020-12-03 08:40:00 +0700

    Add files via upload

---
+++ Bài tập 1/hinhtron.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{ 
+	double DT, CV;
+	double R = 1.5 ;
+	const double PI = 3.14;
+	CV = 2*R*PI;
+	DT = R*R*PI;
+  printf("R: %0.1f\n", R = 1.5);
+  printf("CV hinh tron: %0.1f\n", CV);
+  printf("DT hinh tron: %0.1f\n", DT);
+  getchar();
+  return 0;
+}
