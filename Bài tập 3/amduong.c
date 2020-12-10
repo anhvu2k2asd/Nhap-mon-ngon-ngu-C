520323b22d2276d7cfb8b740a7b58a93f0f2e55c
anhvu2k2asd <72533371+anhvu2k2asd@users.noreply.github.com>
2020-12-10 09:00:02 +0700

    Add files via upload

---
+++ Bài tập 3/amduong.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+	int a = -1;
+	// kiem tra a la duong hay am
+	if (a > 0){
+		printf("%d la so duong",a);
+	}else if(a == 0) {
+		printf("a = 0",a);
+	} else {
+		printf("%d la so am",a);
+		
+	}
+	getchar();
+	return 0;
+}
+ 		
