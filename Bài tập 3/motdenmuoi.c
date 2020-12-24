97e21c40e2958d6a3e4bf45420635423e0ed3d89
anhvu2k2asd <72533371+anhvu2k2asd@users.noreply.github.com>
2020-12-24 09:35:56 +0700

    Add files via upload

---
+++ Bài tập 3/motdenmuoi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	int num;
+	// xuat ra man hinh 1 den 10 tren 1 hang
+	for(num = 1; num <= 10; num = num + 1){
+		printf("%d\t  ", num);
+		}
+	// xuat ra man hinh 1 den 10 tren 10 dong
+	for(num = 1; num <= 10; num = num + 1){
+		printf("%d \n", num);
+		}
+		return 0;
+}
+
