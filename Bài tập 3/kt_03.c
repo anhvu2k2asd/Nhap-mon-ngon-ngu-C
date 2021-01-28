68562421fab9af7be61b2c13dc644c9abbf64fe9
anhvu2k2asd <72533371+anhvu2k2asd@users.noreply.github.com>
2021-01-28 07:37:03 +0000

    Create kt_03.c

---
+++ Bài tập 3/kt_03.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+   int i;
+
+   printf("In cac so le tu 1 den 20:\n");
+  //if ( i%2 != 0 ){
+    for(i = 1; i <= 20; i++) {
+    if ( i%2 != 0 ){
+     if( i==5 || i==11||i==15)
+      continue;
+         printf("%d\n", i);
+    
+    } }
+     return 0;
+}
+#include <stdio.h>
+
+int main() {
+   int i;
+
+   printf("In cac so le tu 1 den 20:\n");
+    i=1;
+    while (i < 20){
+       i++;
+    if ( i%2){
+     switch(i){
+       case 5:
+       case 11:
+       case 15:
+       break;
+     default:
+         printf("%d\n",i);
+     }
+    }
+    } 
+     return 0;
+}
+© 2021 GitHub, Inc.
