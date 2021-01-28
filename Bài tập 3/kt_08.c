ffa7ca71d67bd678a09cf95622e368f61af9d5f9
anhvu2k2asd <72533371+anhvu2k2asd@users.noreply.github.com>
2021-01-28 07:41:18 +0000

    Create kt_08.c

---
+++ Bài tập 3/kt_08.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+int a,b;
+printf("Nhap vao so thuc thu nhat:\n"); 
+scanf("%f",&a); 
+printf("Nhap vao so thuc thu hai:\n"); 
+scanf("%f",&b); 
+printf("%0.1f va %0.1f cung dau", a, b); }
+printf("%0.1f va %0.1f trai dau", a, b);
+}
+return 0;
+}
