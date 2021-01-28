249945c627b4ab7910ac77f0934a7fc2ab2d0bcf
anhvu2k2asd <72533371+anhvu2k2asd@users.noreply.github.com>
2021-01-28 08:35:05 +0000

    Update TH_03.c

--- Bài tập 5/TH_03.c
+++ Bài tập 5/TH_03.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+ 
+int main() 
+{
+    int a, b, c;
+ 
+    printf("Nhap a, b, c \n");
+    scanf("%d%d%d", &a, &b, &c);
+ 
+    int max = a;
+    if(max < b) 
+    {
+        max = b;
+    }
+ 
+    if(max < c) {
+        max = c;
+    }
+ 
+    printf("So lon nhat la %d\n", max);
+ 
+    return 0;
+}
 
