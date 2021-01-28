9e1dfc4e277fad85435823434b4a0fef618abedb
anhvu2k2asd <72533371+anhvu2k2asd@users.noreply.github.com>
2021-01-28 07:37:54 +0000

    Create kt_04.c

---
+++ Bài tập 3/kt_04.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ int main() {
+    int n, i, S = 0;
+    printf("nhap n: ");
+    scanf("%d", &n);
+    if ( n > 0) {
+        while (n != 0) {
+        i = n % 10;
+        S += i;
+        n = n / 10;
+    }
+        printf("tong cac chu so cua n la: %d", S);
+    } else printf("khong the chay chuong trinh!");
+    return 0;
+ }
+© 2021 GitHub, Inc.
