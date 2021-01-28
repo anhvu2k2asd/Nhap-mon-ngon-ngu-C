d4b390213fa0c35cfec4a79f2996ac5a7b02f264
anhvu2k2asd <72533371+anhvu2k2asd@users.noreply.github.com>
2021-01-28 07:32:25 +0000

    Create khoiluong.c

---
+++ Bài tập 2/khoiluong.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+  int m;
+  scanf("%d", &m);
+  printf("m = %dg\n", m * 1000);
+  printf("----------\n");
+  printf("KG  | G\n");
+  printf("----------\n");
+  printf("%d", m);
+  printf("   |%d", m * 1000);
+  return 0;
+}
