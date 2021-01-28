9d5dc7069756f6b155738f120c6f3bb1f35e43dc
anhvu2k2asd <72533371+anhvu2k2asd@users.noreply.github.com>
2021-01-28 07:40:03 +0000

    Create kt_06.c

---
+++ Bài tập 3/kt_06.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+  int h;
+  //h = 4
+  //hàng1:1 ngôi sao;3 khoảng trống//
+  //hàng2:2 ngôi sao;2 khoảng trống//
+  //hàng3:3 ngôi sao;1 khoảng trống//
+  //hàng4:4 ngôi sao;0 khoảng trống//
+  //h = 3
+  //hàng1:2 ngôi sao;2 khoảng trống//
+  //hàng2:3 ngôi sao;1 khoảng trống//
+  //hàng3:4 ngôi sao;0 khoảng trống//
+  //h = 2
+  //hàng1:3 ngôi sao;1 khoảng trống//
+  //hàng2:4 ngôi sao;0 khoảng trống//
+  //h = 1 
+  //hàng1:4 ngôi sao;0 khoảng trống//
+  return 0;
+  
+}
